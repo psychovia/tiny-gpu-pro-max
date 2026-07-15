df67e2a9fa6250efb2ffa3a520e3b3cf030f01b5
psychovia <sapiensnludens@gmail.com>
2026-07-15 10:21:31 -0400

    scheduler description

--- SystemVerilog/scheduler.sv
+++ SystemVerilog/scheduler.sv
@@ -2,4 +2,11 @@
 scheduler
 - manages execution of threads (won't pick up another block before completion)
 - each cpu has it's own register, need to write scheduler that writes the values of the register for cpu
+
+    array indexed by tid
+    element = pc of corresponding thread
+
+    detect different pcs among threads
+        - if different - pick an arbitrary pc to work on, stall the rest, continue when done
+
 **/
